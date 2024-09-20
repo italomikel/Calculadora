b01f301a457d6bef6b34d860063dbcc25a6eccf2
Italo MIkel <138937976+italomikel@users.noreply.github.com>
2024-09-20 17:45:13 -0300

    Initial commit

---
+++ Models/Calculadora.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+
+namespace CalculadoraCsharp.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"O Seno de {angulo} é {Math.Round(seno, 4)}");
+        }
+
+        public void Cos(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"O Coseno de {angulo} é {Math.Round(coseno, 4)}");
+        }
+
+        public void Tan(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"A tangente de {angulo} é {Math.Round(tangente, 4)}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using CalculadoraCsharp.Models;
+
+Calculadora calc = new Calculadora();
+
+//calc.Somar(40, 44);
+//calc.Subtrair(50, 24);
+//calc.Multiplicar(10, 50);
+//calc.Dividir(100, 50);
+//calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Cos(30);
+calc.Tan(30);
